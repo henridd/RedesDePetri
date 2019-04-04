3ca021933286d68e60e642dcb1ad68bad62abca3
Henrique Dalcin Dalmas <henri.Oo@hotmail.com>
2019-04-04 19:54:52 -0300

    Add project files.

---
+++ RedesDePetri/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedesDePetri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
